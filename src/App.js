177c1e94fe571b422a8731193c0cd24d68f5794b
Gotama7 <gotama7@gmail.com>
2025-04-02 08:24:06 +0900

    React Routerの実装を修正

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import QuizApp from './QuizApp';
+import './styles.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<QuizApp />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/*" element={<QuizApp />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles.css';
-import QuizApp from './QuizApp';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <QuizApp />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 ); 
